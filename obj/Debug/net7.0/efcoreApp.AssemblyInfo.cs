a77fc73563fd44ef51b540fa04fc6a035d4b1487
MERVE EKŞİ <merveeksii61@gmail.com>
2024-08-08 20:03:48 +0300

    kurs kayıt formu

--- obj/Debug/net7.0/efcoreApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/efcoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b585417c6c17d1fb5e3a7b928f442a07c4669c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbbd269018e87cf0f18949e75d7185ca1115eaa9")]
 [assembly: System.Reflection.AssemblyProductAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
